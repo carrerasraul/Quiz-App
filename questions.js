0f17f56c832dfaa73083f1221eb4b18df7e8618e
carrerasraul <52898376+carrerasraul@users.noreply.github.com>
2019-10-03 20:50:39 -0400

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,55 @@
+const STORE = {
+    questions: [{
+            question: "What is the name of the sandpaper like material that is on top of the skateboard?".
+            options: [
+                "Griptape",
+                "Sandpaper",
+                "Magic Tape",
+                "Trunks"
+            ],
+            answer: "Griptape"
+        },
+        {
+            question: "Which of the following is NOT a trick you can do on a skateboard",
+            options: [
+                "Nollie Flip",
+                "Backside Smith Grind",
+                "Barspin",
+                "Wallie"
+            ],
+            answer: "Barspin"
+        },
+        {
+            question: "What is called when you do a trick riding backwards on your skateboard?",
+            options: [
+                "Going Backwards",
+                "Fakie",
+                "Nollie",
+                "Not Possible"
+            ],
+            answer: "Fakie"
+        },
+        {
+            question: "Which of the following is considered the “Bible” of skateboarding?",
+            options: [
+                "Transworld Skateboarding",
+                "SLAP Magazine",
+                "Thrasher Magazine",
+                "ESPN Magazine"
+            ],
+            answer: "Thrasher Magazine"
+        },
+        {
+            question: "Which of the following is NOT a proffesional skateboarder?",
+            options: [
+                "Geoff Rowley",
+                "Andrew Reynolds",
+                "Ken Block",
+                "Jake Johnson"
+            ],
+        }
+
+    ],
+    currentQuestion: 0,
+    score: 0
+};
