5764fd51e49b284ea62d22bcaee39efa0133b3b8
carrerasraul <52898376+carrerasraul@users.noreply.github.com>
2019-10-14 00:17:01 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-const startButton = document.getElementById('main-container')
+const startButton = document.getElementById('start')
 const questionContainer = document.getElementById('question-container')
+const nextButton = document.getElementById('next-btn')
+const findOut = document.getElementById('start-container')
+const selectAnswer = document.getElementById('button')
+
 
 startButton.addEventListener('click', startQuiz)
 
@@ -7,35 +11,61 @@ function startQuiz() {
     $('#start').on('click', function(event) {
         displayQuestion();
         startButton.classList.add('hide')
-        questionContainer.classList.remove('hide')
+        findOut.classList.add('hide')
     });
+}
+
+function updateQuestionAndScore() {
+    const html = $(
+        `<div id="quiz-progress-bar" class="quiz-bar">
+            <span class="quiz-progress">Question: <span class="question-num">${STORE.currentQuestion + 1}</span> / ${STORE.questions.length}</span>
+        </div>
+
+
+        <div id="quiz-score" class="score">
+            <span class="number-correct">Current Score: <span class="num-correct">${STORE.score}</span> / ${STORE.questions.length}</span>
+        </div>`);
+    $('.score-and-question').html(html);
 }
 
 function displayQuestion() {
     let question = STORE.questions[STORE.currentQuestion];
-    console.log(question.question)
-    $("#question").html(question.question)
 
-    /*const questionList = $(`<div id="question-container"></div>
-    <p id="question">${question.question}</p>
+    const questionList = $(`<p id="question">${question.question}</p>
     <div id="answer-btns" class="answer-btn">
-        <button class="btn">Answer</button>
-        <button class="btn">Answer</button>
-        <button class="btn">Answer</button>
-        <button class="btn">Answer</button>
-    </div>
-
-    <div class="controls">
-        <button id="start-quiz" class="start-btn">START</button>
-        <button id="next-btn" class="next-btn hide">NEXT</button>
+        <button class="btn">${question.options[0]}</button>
+        <button class="btn">${question.options[1]}</button>
+        <button class="btn">${question.options[2]}</button>
+        <button class="btn">${question.options[3]}</button>
     </div>`);
-    console.log(questionList) */
+
+    $('#question-container').html(questionList)
+    nextButton.classList.remove('hide')
+    updateQuestionAndScore()
+}
+
+function correctAnswer() {
+    if (correct) {
+        Element.classList.add('correct')
+    } else {
+        Element.classList.add('wrong')
+    }
+}
+
+function chooseAnswer() {
+    $('main').on('submit', 'answer-btns', function(event) {
+        let currentQ = STORE.questions[STORE.currentQuestion];
+        let selection = $("input[name=options]:checked").val();
+        let answer = selection.val();
+        let correct = STORE[currentQuestion].answer;
+        if (answer === correct) {
+            correctAnswer()
+        }
+    });
 }
 
 
 
 
-
-
-
+chooseAnswer()
 startQuiz()
