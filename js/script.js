e4f05f8981734b93f3a24b52aa99c3fa5a5263cd
carrerasraul <52898376+carrerasraul@users.noreply.github.com>
2019-10-15 18:42:20 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,18 +2,25 @@ const startButton = document.getElementById('start')
 const questionContainer = document.getElementById('question-container')
 const nextButton = document.getElementById('next-btn')
 const submitButton = document.getElementById('submit')
+const restartButton = document.getElementById('restart')
 const findOut = document.getElementById('start-container')
 const selectAnswer = document.getElementById('button')
 const mainContainer = document.getElementById('main-container')
 const messageBanner = document.getElementById('find-out')
 
 
-let question, answerButtons, selectedAnswer, selectedElement;
+let question,
+    answerButtons,
+    selectedAnswer,
+    selectedElement,
+    correctAnswer;
 
 
 startButton.addEventListener('click', startQuiz)
 submitButton.addEventListener('click', handleCheckAnswer)
 nextButton.addEventListener('click', displayQuestion)
+restartButton.addEventListener('click', finalResults)
+
 
 function startQuiz() {
     $('#start').on('click', function(event) {
@@ -28,8 +35,6 @@ function updateScore() {
         `<div id="quiz-progress-bar" class="quiz-bar">
             <span class="quiz-progress">Question: <span class="question-num">${STORE.currentQuestion + 1 === 6 ? '5' : STORE.currentQuestion + 1}</span> / ${STORE.questions.length}</span>
         </div>
-
-
         <div id="quiz-score" class="score">
             <span class="number-correct">Current Score: <span class="num-correct">${STORE.score}</span> / ${STORE.questions.length}</span>
         </div>`);
@@ -38,9 +43,12 @@ function updateScore() {
 
 function displayQuestion() {
     if (STORE.score === 5) {
-        return messageBanner.textContent = "Congratulations!";
+        finalResults()
     } else {
         question = STORE.questions[STORE.currentQuestion];
+        correctAnswer = question.answer;
+        updateBorder(mainContainer)
+        updateBorder(selectedElement)
         const questionList = $(`<p id="question">${question.question}</p>
         <div id="answer-btns" class="answer-btn">
             <button class="btn">${question.options[0]}</button>
@@ -50,8 +58,11 @@ function displayQuestion() {
         </div>`);
 
         $('#question-container').html(questionList)
+        submitButton.classList.remove('hide')
+        nextButton.classList.add('hide')
         answerButtons = document.getElementById('answer-btns')
         answerButtons.addEventListener('click', handleAnswerSelection)
+        updateScore()
     }
 }
 
@@ -67,7 +78,6 @@ function handleCheckAnswer(evt) {
         STORE.score++
             STORE.currentQuestion < 5 && STORE.currentQuestion++
             selectedAnswer = ""
-            //selectedElement = null /* Why Null? */
         nextButton.classList.add('hide')
         selectedElement.style.border = "3px solid green"
         mainContainer.style.border = "5px solid green"
@@ -77,35 +87,43 @@ function handleCheckAnswer(evt) {
         submitButton.classList.add('hide')
         nextButton.classList.remove('hide')
     } else {
-        //STORE.currentQuestion < 5 && 
-        //STORE.currentQuestion++
-        selectedAnswer = ""
+        STORE.currentQuestion < 5 &&
+            STORE.currentQuestion++
+            selectedAnswer = ""
         selectedElement.style.border = "3px solid red"
         mainContainer.style.border = "5px solid red"
-        let feedbackWrong = `<section class="result-box-wrong">Wrong! The correct answer is "${STORE.questions[STORE.currentQuestion].answer}"</section>`
+        let feedbackWrong = `<section class="result-box-wrong">Wrong! The correct answer is "${correctAnswer}"</section>`
         $('#answer-btns').append(feedbackWrong)
+        correctAnswer = "";
         submitButton.classList.add('hide')
         nextButton.classList.remove('hide')
     }
 }
 
-/*function updateOptions()
-{
-  let question = STORE.questions[STORE.currentQuestion];
-  for(let i=0; i<question.options.length; i++)
-  {
-    $('.js-options').append(`
-        <p id="question">${i + 1}</p>
-        <div id="answer-btns" class="answer-btn">
-            <button class="btn">${question.options + 1 [0]}</button>
-            <button class="btn">${question.options + 1 [1]}</button>
-            <button class="btn">${question.options + 1 [2]}</button>
-            <button class="btn">${question.options + 1 [3]}</button>
-        </div>
-    `);
-  }
-  
-} */
+function updateBorder(element) {
+    if (element) {
+        setTimeout(function() {
+            element.style.border = "3px solid black"
+        }, 1000)
+    }
+}
 
+function finalResults() {
+    let resultsScreen = $(
+        `<div id="question-container">
+            <p id="complete">COMPLETE!</p>
+
+            <p>You Scored: ${STORE.score} / ${STORE.questions.length}</p>
+
+            <div class="controls">
+                <button id="restart" class="restart-btn">RESTART QUIZ</button></div> </div>`);
+    STORE.currentQuestion = 0;
+    STORE.score = 0;
+    $("question-container").html(resultsScreen);
+}
+
+function restartQuiz() {
+    window.location.reload()
+}
 
 startQuiz()
